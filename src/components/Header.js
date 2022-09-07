86a4d404b2fe27ff5c4fc3668331983dc3d54768
Yoann <yos59540@gmail.com>
2022-09-07 19:13:57 +0200

    feat: ajout des images des plats et de la structure html

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,24 +2,27 @@ import * as React from 'react';
 import { Link } from 'react-router-dom';
 import test from "../assets/img/logo_header.svg"
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import "../assets/css/index.css"
 
 function Header() {
   return (
-    <header className='header'>
-      <div className='logo_netfood'>
-        <Link to="/">
-          <img className="logo_header"
+    <React.Fragment>
+      <div className='container_header'>
+        <div className='logo_header'>
+          <Link to="/">
+            <img className="logo_netfood"
               src={test}
               alt="Logo_NetFood"
-          />
-        </Link>
+            />
+          </Link>
+        </div>
+        <div>
+          <AccountCircleIcon />
+        </div>
       </div>
-        <AccountCircleIcon/>
-      <div>
-          
-      </div>
-    </header>
+    </React.Fragment>
 
-    );
+
+  );
 }
 export default Header;
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,8 +1,90 @@
 import * as React from 'react';
+import italien1 from "../assets/img/lasagne.jpg"
+import italien2 from "../assets/img/involtinis.jpg"
+import italien3 from "../assets/img/muffuletta.jpg"
+import indien1 from "../assets/img/pizza.jpeg"
+import indien2 from "../assets/img/omelette.jpeg"
+import indien3 from "../assets/img/kefta.jpeg"
 
 export default function main() {
-    return(
-        <p>main</p>
+    return (
+        <React.Fragment>
+            <div>
+                <div>
+                    <div>
+                        <h1>Italien</h1>
+                        <img className="img_italien1"
+                            src={italien1}
+                            alt="Lasagne Italienne"
+                        />
+                        <h3>Lasagne Italienne</h3>
+                    </div>
+                    <div>
+                        <img className="img_italien2"
+                            src={italien2}
+                            alt="Involtinis au poulet champignons et mozzarella"
+                        />
+                        <h3>Involtinis au poulet champignons et mozzarella</h3>
+                    </div>
+                    <div>
+                        <img className="img_italien3"
+                            src={italien3}
+                            alt="Muffuletta sandwich sicilien au salami mortadelle et provolone"
+                        />
+                        <h3>Muffuletta sandwich sicilien au salami mortadelle et provolone</h3>
+                    </div>
+                </div>
+                <div>
+                    <div>
+                        <h1>Indien</h1>
+                        <img className="img_indien1"
+                            src={indien1}
+                            alt="Pizza à l'Indienne"
+                        />
+                        <h3>Pizza à l'Indienne</h3>
+                    </div>
+                    <div>
+                        <img className="img_indien2"
+                            src={indien2}
+                            alt="Omelette indienne"
+                        />
+                        <h3>Omelette indienne</h3>
+                    </div>
+                    <div>
+                        <img className="img_indien3"
+                            src={indien3}
+                            alt="Kefta à l'indienne"
+                        />
+                        <h3>Kefta à l'indienne</h3>
+                    </div>
+                </div>
+                <div>
+                    <div>
+                        <h1>Italien</h1>
+                        <img className="logo_netfood"
+                            src={italien1}
+                            alt="Lasagne Italienne"
+                        />
+                        <h3>Lasagne Italienne</h3>
+                    </div>
+                    <div>
+                        <img className="logo_netfood"
+                            src={italien2}
+                            alt="Involtinis au poulet champignons et mozzarella"
+                        />
+                        <h3>Involtinis au poulet champignons et mozzarella</h3>
+                    </div>
+                    <div>
+                        <img className="logo_netfood"
+                            src={italien3}
+                            alt="Muffuletta sandwich sicilien au salami mortadelle et provolone"
+                        />
+                        <h3>Muffuletta sandwich sicilien au salami mortadelle et provolone</h3>
+                    </div>
+                </div>
+                
+            </div>
+        </React.Fragment>
     )
 }
 
