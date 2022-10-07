0533677cd74716a2c7d27fc73727b6b161186839
Yoann VENTURA <yos59540@gmail.com>
2022-10-07 14:09:22 +0200

    feat: affichage des données de la BDD et amélioration du header

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,33 +1,28 @@
-import * as React from 'react';
-import axios from 'axios'
-import "../assets/css/index.css"
-import italien1 from "../assets/img/lasagne.jpg"
-import italien2 from "../assets/img/involtinis.jpg"
-import italien3 from "../assets/img/muffuletta.jpg"
-import indien1 from "../assets/img/pizza.jpeg"
-import indien2 from "../assets/img/omelette.jpeg"
-import indien3 from "../assets/img/kefta.jpeg"
-import { useState } from 'react';
+import * as React from "react";
+import axios from "axios";
+import "../assets/css/index.css";
+import italien1 from "../assets/img/lasagne.jpg";
+import italien2 from "../assets/img/involtinis.jpg";
+import italien3 from "../assets/img/muffuletta.jpg";
+import indien1 from "../assets/img/pizza.jpeg";
+import indien2 from "../assets/img/omelette.jpeg";
+import indien3 from "../assets/img/kefta.jpeg";
+import { useState } from "react";
+import { useEffect } from "react";
 
 export default function main() {
+  const URLPlats = "http://localhost:5000/plats";
 
   const [modal, setModal] = useState(false);
-  const [validModal, setValidModal] = useState(false)
+  const [validModal, setValidModal] = useState(false);
+  const [plats, setPlats] = useState([]);
 
-  // const testApi = () => {
-  //   fetch('http://localhost:5000/plats')
-  //  .then(response => response.json())
-  //  .then(data => console.log(data));
-  // }
-
-  const getPlats = () => {
-    axios.get(`http://localhost:5000/plats`)
-      .then(res => {
-        console.log(res)
-      })
-  }
-
-  getPlats()
+  useEffect(() => {
+    axios.get(URLPlats).then((response) => {
+      setPlats(response.data);
+      console.log("Plats :", response.data);
+    });
+  }, []);
 
   const handleOpen = () => {
     setModal(true);
@@ -35,48 +30,43 @@ export default function main() {
 
   const handleClose = () => {
     setModal(false);
-    setValidModal(false)
+    setValidModal(false);
   };
 
   const handleValidModal = () => {
-    setValidModal(true)
-    setModal(false)
-  }
-
-
+    setValidModal(true);
+    setModal(false);
+  };
 
   return (
     <React.Fragment>
-
-      {validModal &&
+      {validModal && (
         <div className="overlay">
           <div className="modal">
             <div className="modal-content">
-              <h2 className='titre_modal'>Plat commandée</h2>
+              <h2 className="titre_modal">Plat commandée</h2>
               <p>
                 Votre plat est validé ! Votre commande sera bientôt expédiée.
               </p>
               <p>Bonne dégustation.</p>
             </div>
-            <div className='modal-content2'>
+            <div className="modal-content2">
               <button onClick={handleClose} className="close-modal">
                 RETOUR
               </button>
             </div>
           </div>
         </div>
-      }
+      )}
 
-      {modal &&
+      {modal && (
         <div className="overlay">
           <div className="modal">
             <div className="modal-content">
-              <h2 className='titre_modal'>Confirmation de votre plat</h2>
-              <p>
-                Souhaitez-vous commander ce plat ?
-              </p>
+              <h2 className="titre_modal">Confirmation de votre plat</h2>
+              <p>Souhaitez-vous commander ce plat ?</p>
             </div>
-            <div className='modal-content2'>
+            <div className="modal-content2">
               <button onClick={handleValidModal} className="open-new-modal">
                 VALIDER
               </button>
@@ -86,104 +76,32 @@ export default function main() {
             </div>
           </div>
         </div>
-      }
+      )}
 
-      <div className='container_main'>
-        <div className='theme'>
-          <h3 className='titre_theme'>Italien</h3>
-          <div className='plat'>
-            <a onClick={handleOpen} className="btn-modal">
-              <figure>
-                <img className="img_plat"
-                  src={italien1}
-                  alt="Lasagne Italienne"
-                />
-                <figcaption>Lasagne Italienne</figcaption>
-              </figure>
-            </a>
+      
+
+        <div className="container_main">
+        {plats.map((plat) => (
+          <div key={plat._id} className="theme">
+            <h3 className="titre_theme">{plat.theme_plat}</h3>
+            <div className="plat">
+              <span onClick={handleOpen} className="btn-modal">
+                <figure>
+                  <img
+                    className="img_plat"
+                    src={plat.illustration_plat}
+                    alt="{plat.nom__plat}"
+                  />
+                  <figcaption>
+                    {plat.nom__plat}
+                  </figcaption>
+                </figure>
+              </span>
+            </div>
           </div>
-          <div className='plat'>
-            <figure>
-              <img className="img_plat"
-                src={italien2}
-                alt="Involtinis au poulet champignons et mozzarella"
-              />
-              <figcaption>Involtinis au poulet champignons et mozzarella</figcaption>
-            </figure>
-          </div>
-          <div className='plat'>
-            <figure>
-              <img className="img_plat"
-                src={italien3}
-                alt="Muffuletta sandwich sicilien au salami mortadelle et provolone"
-              />
-              <figcaption>Muffuletta sandwich sicilien au salami mortadelle et provolone</figcaption>
-            </figure>
-          </div>
-        </div>
-        <div className='theme'>
-          <h3 className='titre_theme'>Indien</h3>
-          <div className='plat'>
-            <figure>
-              <img className="img_plat"
-                src={indien1}
-                alt="Pizza à l'Indienne"
-              />
-              <figcaption>Pizza à l'Indienne</figcaption>
-            </figure>
-          </div>
-          <div className='plat'>
-            <figure>
-              <img className="img_plat"
-                src={indien2}
-                alt="Omelette indienne"
-              />
-              <figcaption>Omelette indienne</figcaption>
-            </figure>
-          </div>
-          <div className='plat'>
-            <figure>
-              <img className="img_plat"
-                src={indien3}
-                alt="Kefta à l'indienne"
-              />
-              <figcaption>Kefta à l'indienne</figcaption>
-            </figure>
-          </div>
-        </div>
-        <div className='theme'>
-          <h3 className='titre_theme'>Indien</h3>
-          <div className='plat'>
-            <figure>
-              <img className="img_plat"
-                src={indien1}
-                alt="Pizza à l'Indienne"
-              />
-              <figcaption>Pizza à l'Indienne</figcaption>
-            </figure>
-          </div>
-          <div className='plat'>
-            <figure>
-              <img className="img_plat"
-                src={indien2}
-                alt="Omelette indienne"
-              />
-              <figcaption>Omelette indienne</figcaption>
-            </figure>
-          </div>
-          <div className='plat'>
-            <figure>
-              <img className="img_plat"
-                src={indien3}
-                alt="Kefta à l'indienne"
-              />
-              <figcaption>Kefta à l'indienne</figcaption>
-            </figure>
-          </div>
-        </div>
+      ))}
       </div>
-
+      
     </React.Fragment>
-  )
+  );
 }
-
